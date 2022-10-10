8025ca0d3ed093eab547e1762b0c44b2568403e9
C. Serrano <serrano.cjm@gmail.com>
2022-10-10 09:15:43 -0400

    first commit

---
+++ app/src/main/java/moe/cachapa/android/mvvm/template/MainActivity.kt
@@ -0,0 +1,11 @@
+package moe.cachapa.android.mvvm.template
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
