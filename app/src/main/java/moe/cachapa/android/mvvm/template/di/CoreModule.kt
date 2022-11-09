4a2085ff7b62bf8c674fb08b92656db29c1e5933
C. Serrano <serrano.cjm@gmail.com>
2022-11-09 10:56:21 -0400

    ktlint

--- app/src/main/java/moe/cachapa/android/mvvm/template/di/CoreModule.kt
+++ app/src/main/java/moe/cachapa/android/mvvm/template/di/CoreModule.kt
@@ -1,11 +1,12 @@
 package moe.cachapa.android.mvvm.template.di
 
-import moe.cachapa.android.mvvm.template.di.modules.*
+import moe.cachapa.android.mvvm.template.di.modules.* // ktlint-disable no-wildcard-imports
 import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
 
 object CoreModule {
-    private val modules =
-        listOf(
+    private val modules: List<Module>
+        get() = listOf(
             networkModule,
             repositoryModule,
             localModule,
